494961df777e8000eaa2732ea3f6e64494a81fe1
Jose Magana <josemaganadev@gmail.com>
2015-08-09 22:56:23 -0700

    take all fields as text

--- ship/schema.sql
+++ ship/schema.sql
@@ -1,7 +1,7 @@
 drop table if exists deliveries;
 create table deliveries (
-    tracking integer primary key,
+    tracking text primary key,
     carrier text not null,
     street_address text not null,
-    zipcode integer not null
+    zipcode text not null
 );
