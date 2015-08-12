d0ca5e497cce8d785afed97dac83c2e592fdd67a
Jose Magana <josemaganadev@gmail.com>
2015-08-12 09:34:32 -0700

    wrong database location

--- ship/ship.py
+++ ship/ship.py
@@ -3,7 +3,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 from contextlib import closing
 
-DATABASE = 'tmp/ship.db'
+DATABASE = 'ship/tmp/ship.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
